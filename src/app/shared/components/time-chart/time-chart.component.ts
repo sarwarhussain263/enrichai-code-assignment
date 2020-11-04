97f243bc2b7c7eb0aed0a56f984f5bc8f79bec87
Sarwar Hussain <sarwarhussain263@live.com>
2020-11-04 11:35:50 +0530

    'hosting'

--- src/app/shared/components/time-chart/time-chart.component.ts
+++ src/app/shared/components/time-chart/time-chart.component.ts
@@ -38,24 +38,24 @@ export class TimeChartComponent implements OnInit {
       let chart = am4core.create("timeChartDiv", am4charts.XYChart);
 
       chart.data = [{
-        "country": "USA",
-        "visits": 2025
+        "trip": "USA",
+        "km": 40
       }, {
-        "country": "China",
-        "visits": 1882
+        "trip": "China",
+        "km": 20
       }, {
-        "country": "Japan",
-        "visits": 1809
+        "trip": "Japan",
+        "km": 50
       }, {
-        "country": "Germany",
-        "visits": 1322
+        "trip": "Germany",
+        "km": 30
       }];
 
       //chart.padding(40, 40, 40, 40);
 
       let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
       categoryAxis.renderer.grid.template.location = 0;
-      categoryAxis.dataFields.category = "country";
+      categoryAxis.dataFields.category = "trip";
       categoryAxis.renderer.minGridDistance = 60;
       categoryAxis.renderer.inversed = true;
       categoryAxis.renderer.grid.template.disabled = true;
@@ -63,18 +63,18 @@ export class TimeChartComponent implements OnInit {
       let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
       valueAxis.min = 0;
       valueAxis.extraMax = 0.1;
+      valueAxis.title.text = "KM"
       //valueAxis.rangeChangeEasing = am4core.ease.linear;
       //valueAxis.rangeChangeDuration = 1500;
 
       let series = chart.series.push(new am4charts.ColumnSeries());
-      series.dataFields.categoryX = "country";
-      series.dataFields.valueY = "visits";
+      series.dataFields.categoryX = "trip";
+      series.dataFields.valueY = "km";
       series.tooltipText = "{valueY.value}"
       series.columns.template.strokeOpacity = 0;
-      series.columns.template.column.cornerRadiusTopRight = 10;
-      series.columns.template.column.cornerRadiusTopLeft = 10;
-      //series.interpolationDuration = 1500;
-      //series.interpolationEasing = am4core.ease.linear;
+    
+      series.columns.template.width = am4core.percent(17);
+      
       let labelBullet = series.bullets.push(new am4charts.LabelBullet());
       labelBullet.label.verticalCenter = "bottom";
       labelBullet.label.dy = -10;
